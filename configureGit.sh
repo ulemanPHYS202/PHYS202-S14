24bbb482f17c40253cc835299916d7e0a139f0a7
Matthijs Uleman <uleman@csm-180-272-20.ad.calpoly.edu>
2014-04-03 13:11:24 -0700

    1st commit

---
+++ configureGit.sh
@@ -0,0 +1,8 @@
+#!/binbash
+
+#script configures git on multiple computers. run when working on multiple computers
+
+gitconfig --global user.name "Matt Uleman"
+gitconfig --global user.email "deltoram@aol.com"
+gitconfig --global color.ui "auto"
+gitconfig --global core.editor "nano"
