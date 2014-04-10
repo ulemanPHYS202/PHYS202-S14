4f9d0907791dd25d944c844b7ab0f4db6caf09e2
Matt Uleman <deltoram@aol.com>
2014-04-10 13:29:19 -0700

    practice stuff using modules and magic fxns

---
+++ iPython/mymodule.py
@@ -0,0 +1,8 @@
+#demonstration of modules son
+
+def add_numbers(x,y):
+    """docstrings are important in modules"""
+    return x+y
+def subtract_numbers(x,y):
+    """subtract y from x yo"""
+    return x-y
